600f687f3dc5dabc86c02c0d7812c7e10d996b74
HCore <support@hwow.ru>
2017-07-17 15:39:35 +0500

    Изменила коперайты

--- modules/worldengine/nucleus/src/SystemConfig.h
+++ modules/worldengine/nucleus/src/SystemConfig.h
@@ -12,7 +12,7 @@
 #include "Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "AzerothCore"
+#define _PACKAGENAME "History WoW"
 
 #if TRINITY_ENDIAN == TRINITY_BIGENDIAN
 # define _ENDIAN_STRING "big-endian"
